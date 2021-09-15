61fa11c5848b7e10e1e6f1d4732a832f7c481bbf
Fallen Starrr <juzopigeon@gmail.com>
2021-09-15 12:29:28 +0600

    Carousel

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { ExampleComment } from "./ExampleComment";
 import {
   UserOutlined,
@@ -20,8 +22,7 @@ import {
   Form,
   Button,
   List,
-  Input,
-  Carousel,
+  Input
 } from "antd";
 import "antd/dist/antd.css";
 import "../styles/post.css";
@@ -88,20 +89,45 @@ export default function Post() {
           </Form.Item>
         </Col>
       </Row>
-      {/* <Carousel autoplay>
-    <div>
-      <h3 style={contentStyle}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-  </Carousel> */}
+     
+      <Carousel className="w-50 m-auto">
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="image 4.png"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="image 4.png"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="image 4.png"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+     
     </React.Fragment>
   );
 }
