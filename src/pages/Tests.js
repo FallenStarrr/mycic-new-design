cb8931186c834b2221b3805755bd55288d4c237a
Fallen Starrr <juzopigeon@gmail.com>
2021-09-23 15:42:43 +0600

    tabs layout fix

--- src/pages/Tests.js
+++ src/pages/Tests.js
@@ -17,117 +17,40 @@ const Tests = () => {
         <h1>Админка</h1>
         <hr />
 
-        <button
-          className="nav-link active"
-          id="v-pills-home-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#v-pills-home"
-          type="button"
-          role="tab"
-          aria-controls="v-pills-home"
-          aria-selected="true"
-        >
-          Назначение обучения
-        </button>
-        <button
-          className="nav-link"
-          id="v-pills-profile-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#v-pills-profile"
-          type="button"
-          role="tab"
-          aria-controls="v-pills-profile"
-          aria-selected="false"
-        >
-          Хочу провести обучение
-        </button>
-        <button
-          className="nav-link"
-          id="v-pills-messages-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#v-pills-messages"
-          type="button"
-          role="tab"
-          aria-controls="v-pills-messages"
-          aria-selected="false"
-        >
-          Хочу пройти обучение
-        </button>
-        <button
-          className="nav-link"
-          id="v-pills-settings-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#v-pills-settings"
-          type="button"
-          role="tab"
-          aria-controls="v-pills-settings"
-          aria-selected="false"
-        >
-          Загрузки
-        </button>
-        <button
-          className="nav-link"
-          id="v-pills-announcements-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#v-pills-announcements"
-          type="button"
-          role="tab"
-          aria-controls="v-pills-announcements"
-          aria-selected="false"
-        >
-          Анонсы обучения
-        </button>
-        <button
-          className="nav-link"
-          id="v-pills-res-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#v-pills-res"
-          type="button"
-          role="tab"
-          aria-controls="v-pills-res"
-          aria-selected="false"
-        >
-          Отчеты
-        </button>
-        <button
-          className="nav-link"
-          id="v-pills-download-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#v-pills-download"
-          type="button"
-          role="tab"
-          aria-controls="v-pills-download"
-          aria-selected="false"
-        >
-          Программа обучения
-        </button>
+        <Tabs defaultActiveKey="1" className="m-auto">
+          <TabPane tab="Анонсы обучения" key="1">
+            <Announcement />
+          </TabPane>
+          <TabPane tab=" Назначение обучения" key="2">
+            <TakeLesson />
+          </TabPane>
+          <TabPane tab="Хочу провести обучение" key="3">
+            <WantTeach />
+          </TabPane>
+          <TabPane tab="Хочу пройти обучение" key="4">
+            <WannaLearn />
+          </TabPane>
+
+          <TabPane
+            tab=" Отчеты
+        "
+            key="5"
+          >
+            <Report />
+          </TabPane>
+          <TabPane tab=" Программа обучения" key="6">
+            <StudyProgram />
+          </TabPane>
+
+          <TabPane
+            tab="
+          Загрузки"
+            key="7"
+          >
+            <Download />
+          </TabPane>
+        </Tabs>
       </div>
-
-      <Tabs defaultActiveKey="1" className="m-auto">
-        <TabPane tab="Tab 1" key="1">
-          <Announcement />
-        </TabPane>
-        <TabPane tab="Tab 2" key="2">
-          <TakeLesson />
-        </TabPane>
-        <TabPane tab="Tab 3" key="3">
-          <WantTeach />
-        </TabPane>
-        <TabPane tab="Tab 4" key="4">
-          <WannaLearn />
-        </TabPane>
-
-        <TabPane tab="Tab 5" key="5">
-          <Report />
-        </TabPane>
-        <TabPane tab="Tab 6" key="6">
-          <StudyProgram />
-        </TabPane>
-
-        <TabPane tab="Tab 7" key="7">
-          <Download />
-        </TabPane>
-      </Tabs>
     </>
   );
 };
