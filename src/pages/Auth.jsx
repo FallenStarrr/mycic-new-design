d2f1b1a42c7d3d05efd32a7ee6006be57d24e723
Fallen Starrr <juzopigeon@gmail.com>
2021-11-03 14:50:26 +0600

    icons restore eye , logo, users

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -4,6 +4,8 @@ import $api from "../http-axios";
 import axios from "axios";
 import './main.css';
 import './app.css';
+import users from './users-white-image.png'
+import logo from './new-white-logo.png'
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -30,7 +32,7 @@ const Auth = () => {
             <div className='col-lg-12'>
               <div className='header'>
                 <img
-                  src='./My.cic.kz_files/new-white-logo.png'
+                  src={logo}
                   alt='Centras Logo'
                   className='header__logo'
                 />
@@ -75,7 +77,7 @@ const Auth = () => {
             <div className='col-lg-6 column-2'>
               <div className='users d-flex justify-content-center'>
                 <img
-                  src='./My.cic.kz_files/users-white-image.png'
+                  src={users}
                   alt='users image'
                   className='users__image'
                 />
@@ -106,8 +108,13 @@ const Auth = () => {
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                     />
+                   
                     <button type='button' className='show-btn'>
-                      <i aria-hidden='true' id='icon' className='fa fa-eye'></i>
+                    <svg aria-hidden='true' id='icon' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye"
+                     viewBox="0 0 16 16">
+                   <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                    </svg>
                     </button>
                   </div>
                 </div>
